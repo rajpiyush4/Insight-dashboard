5928dc778d0d98235738bd89a822aa9c04d0025a
Piyush Raj <86086085+rajpiyush4@users.noreply.github.com>
2024-01-29 11:31:00 +0530

    Updated Vendors page

--- src/components/Vendors.tsx
+++ src/components/Vendors.tsx
@@ -24,8 +24,10 @@ function Vendors({isShow}:{isShow:boolean}) {
   }
   return (
     <section className={`${isShow?"w-[95%]": 'w-[80%]' } h-[100vh] p-4 overflow-y-scroll`} >
-      <div className="font-bold text-lg text-center bg-[#eee] p-4 mb-4">Vendors</div>
-     {data[1]?.map((item:string, key:number)=> key != 0 && <div className="bg-[#212121] text-center mb-4 p-4 cursor-pointer rounded-md shadow-md text-white w-fit border-2 m-auto duration-75 hover:-translate-y-1" key={key}>{item}</div>)}
+      <div className="font-bold text-3xl p-4 mb-4">Vendors</div>
+      <ul className="bg-[white] p-4 rounded-md underline">
+     {data[1]?.map((item:string, key:number)=> key != 0 && <li className=" mb-4 p-4 cursor-pointer rounded-md m-auto duration-75 hover:bg-[whitesmoke]" key={key}>{item}</li>)}
+      </ul>
     </section >
   )
 }
